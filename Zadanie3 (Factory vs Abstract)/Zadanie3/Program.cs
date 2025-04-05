31cb7db0eb9980458ee61580d95386cba56c5375
Patryk Kotulski <102411007+kotulski@users.noreply.github.com>
2025-04-05 16:53:23 +0200

    Add files via upload

---
+++ Zadanie3 (Factory vs Abstract)/Zadanie3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+public interface IPaczka
+{
+    void Przygotuj();
+}
+
+class MalaPaczka : IPaczka
+{
+    public void Przygotuj()
+    {
+        Console.WriteLine("Przygotowano małą paczkę");
+    }
+}
+
+class DuzaPaczka : IPaczka
+{
+    public void Przygotuj()
+    {
+        Console.WriteLine("Przygotowano dużą paczkę");
+    }
+}
+
+class GigantycznaPaczka : IPaczka
+{
+    public void Przygotuj()
+    {
+        Console.WriteLine("Przygotowano gigantyczną paczkę");
+    }
+}
+
+public interface IFabrykaPaczek
+{
+    IPaczka UtworzPaczke();
+}
+
+class FabrykaDuzychPaczek : IFabrykaPaczek
+{
+    public IPaczka UtworzPaczke()
+    {
+        return new DuzaPaczka();
+    }
+}
+
+class FabrykaMalychPaczek : IFabrykaPaczek
+{
+    public IPaczka UtworzPaczke()
+    {
+        return new MalaPaczka();
+    }
+}
+
+class FabrykaGigantycznychPaczek : IFabrykaPaczek
+{
+    public IPaczka UtworzPaczke()
+    {
+        return new GigantycznaPaczka();
+    }
+}
+
+class ZarzadzaniePaczkami
+{
+    private IFabrykaPaczek fabrykaPaczek;
+    private static ZarzadzaniePaczkami _instancja;
+
+    private ZarzadzaniePaczkami() { }
+
+    public static ZarzadzaniePaczkami Instancja
+    {
+        get
+        {
+            if (_instancja == null)
+            {
+                _instancja = new ZarzadzaniePaczkami();
+            }
+            return _instancja;
+        }
+    }
+
+    public void UstawFabryke(IFabrykaPaczek fabryka)
+    {
+        fabrykaPaczek = fabryka;
+    }
+
+    public void PrzygotujPaczke()
+    {
+        if (fabrykaPaczek == null)
+        {
+            Console.WriteLine("Nie ustawiono fabryki paczek.");
+            return;
+        }
+
+        var paczka = fabrykaPaczek.UtworzPaczke();
+        paczka.Przygotuj();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var zarzadzanie = ZarzadzaniePaczkami.Instancja;
+
+        zarzadzanie.UstawFabryke(new FabrykaMalychPaczek());
+        zarzadzanie.PrzygotujPaczke();
+
+        zarzadzanie.UstawFabryke(new FabrykaDuzychPaczek());
+        zarzadzanie.PrzygotujPaczke();
+
+        zarzadzanie.UstawFabryke(new FabrykaGigantycznychPaczek());
+        zarzadzanie.PrzygotujPaczke();
+
+        Console.ReadLine();
+    }
+}
---
+++ Zadanie4 (Kurierzy)/Zadanie4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Logistyka
+{
+    // Interfejsy produktów
+    public interface IPaczka
+    {
+        void Spakuj();
+    }
+
+    public interface IKurier
+    {
+        void Dostarcz();
+    }
+
+    // Implementacje produktów
+    public class MalaPaczka : IPaczka
+    {
+        public void Spakuj() => Console.WriteLine("Spakowano MAŁĄ paczkę.");
+    }
+
+    public class DuzaPaczka : IPaczka
+    {
+        public void Spakuj() => Console.WriteLine("Spakowano DUŻĄ paczkę.");
+    }
+
+    public class DHLKurier : IKurier
+    {
+        public void Dostarcz() => Console.WriteLine("Dostarczono przez kuriera DHL.");
+    }
+
+    public class UPSKurier : IKurier
+    {
+        public void Dostarcz() => Console.WriteLine("Dostarczono przez kuriera UPS.");
+    }
+
+    // Interfejs Abstract Factory
+    public interface IFabrykaLogistyki
+    {
+        IPaczka UtworzPaczke();
+        IKurier UtworzKuriera();
+    }
+
+    // Konkretne fabryki
+    public class FabrykaLogistykiPolska : IFabrykaLogistyki
+    {
+        public IPaczka UtworzPaczke() => new MalaPaczka();
+        public IKurier UtworzKuriera() => new DHLKurier();
+    }
+
+    public class FabrykaLogistykiUSA : IFabrykaLogistyki
+    {
+        public IPaczka UtworzPaczke() => new DuzaPaczka();
+        public IKurier UtworzKuriera() => new UPSKurier();
+    }
+
+    // Singleton zarządzający przesyłkami
+    public class ZarzadzaniePrzesylkami
+    {
+        private static ZarzadzaniePrzesylkami _instancja;
+        private IFabrykaLogistyki fabrykaLogistyki;
+
+        private ZarzadzaniePrzesylkami() { }
+
+        public static ZarzadzaniePrzesylkami Instancja
+        {
+            get
+            {
+                if (_instancja == null)
+                    _instancja = new ZarzadzaniePrzesylkami();
+                return _instancja;
+            }
+        }
+
+        // Metoda obsługująca zamówienie
+        public void PrzyjmijZamowienie(string lokalizacja) // Funkcja string 
+        {
+            if (lokalizacja.ToLower() == "polska")
+                fabrykaLogistyki = new FabrykaLogistykiPolska();
+            else if (lokalizacja.ToLower() == "usa")
+                fabrykaLogistyki = new FabrykaLogistykiUSA();
+            else
+            {
+                Console.WriteLine("Nieobsługiwana lokalizacja.");
+                return;
+            }
+
+            var paczka = fabrykaLogistyki.UtworzPaczke();
+            var kurier = fabrykaLogistyki.UtworzKuriera();
+
+            paczka.Spakuj();
+            kurier.Dostarcz();
+            Console.WriteLine("Zamówienie zrealizowane dla: " + lokalizacja + "\n");
+        }
+    }
+
+    // Testowanie programu 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ZarzadzaniePrzesylkami.Instancja;
+
+            system.PrzyjmijZamowienie("Polska");  // Mała paczka + DHL
+            system.PrzyjmijZamowienie("USA");     // Duża paczka + UPS
+            system.PrzyjmijZamowienie("Niemcy");  // Nieobsługiwane
+
+            Console.ReadLine();
+        }
+    }
+}
